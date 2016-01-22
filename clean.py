cf9a8d5cb2764423ba0936b2db35a79d8da00c3f
Carlos Barajas <12barajasc@gmail.com>
2016-01-22 18:36:36 -0500

    Clean up, and read in data

---
+++ clean.py
@@ -0,0 +1,11 @@
+from numpy import array
+
+def generate_array(kplrfile):
+    """(str) -> NumPy Array
+
+
+    Takes the location of a kplrfile and returns a multi dimension array. Also 
+    returns a dictionary of "defaults"""
+    # Read in files
+    # Read & Generate defaults
+    # Read & Generate values array dtype={name:[column1, column2]}
