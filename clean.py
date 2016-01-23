7d522e56c2ffc985b32562500f532af6035761be
Carlos Barajas <12barajasc@gmail.com>
2016-01-23 02:24:47 -0500

    still fuzzy about doing it this way

--- clean.py
+++ clean.py
@@ -1,11 +1,26 @@
 from numpy import array
 
 def array_from_file(kplrfile):
-    """(str) -> NumPy Array
+    """(str) -> NumPy Array, dict
 
 
     Takes the location of a kplrfile and returns a multi dimension array. Also 
-    returns a dictionary of "defaults"""
+    returns and dictionary of "defaults"""
     # Read in files
+    with open(kplrfile, 'r') as kepler_dat:
+        defaults = {}
+        data = {}
+        for line in kepler_dat:
+            line = line.strip()
+            if line[0] == "\\":
+                line = line.split('=')
+                if len(line) < 2:
+                    defaults[line[0]] = None
+                else:
+                    defaults[line[0]] = line[1]
+                    # Determine type, str, and convert correctly
+            elif line[0] == '|':
+                line = line.split()
+                
     # Read & Generate defaults
     # Read & Generate values array dtype={name:[column1, column2]}
