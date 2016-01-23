ad4cddaeabe8bb82ea8a79788cbf79848779eb38
Carlos Barajas <12barajasc@gmail.com>
2016-01-23 04:23:19 -0500

    "done". Should change if statement order later

--- testlabs.py
+++ testlabs.py
@@ -10,16 +10,21 @@ def into_db(filename):
     current_db.insert("""INSERT INTO files VALUES (%s, %s);""", [filename, datetime.now()])
     with open(filename, 'r') as kepler_file:
         for line in kepler_file:
+            # Handles header data
             if line[0] == "\\":
-                # Do stuff
+                line = line[1:].split("=")
                 if "KEPLERID" == line[0]:
                     kep_id = line[1]
                 elif "QUARTER" == line[0]:
                     quarter = line[1]
-            elif line[0] == "|":
-                # Do stuff
+            # Skips the three table headers
+            elif "|" in line:
+                continue
+                # line = line[1:-1].replace("|"," ").split()
+            # Handles the EOF and perhaps bizzare line breaks
             elif len(line) < 5:
                 continue
+            # The important stuff
             else:
                 # Insert data into the database
                 line = line.split()
