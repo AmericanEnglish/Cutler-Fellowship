afe47152d61e6cad58a7ba24b7864e811f4050a9
Carlos Barajas <12barajasc@gmail.com>
2016-01-23 22:45:03 -0500

    Finished putting the data into sqlite

--- testlabs.py
+++ testlabs.py
@@ -1,18 +1,36 @@
 from database import DB
 from datetime import datetime
+
 def into_db(filename):
-    current_db = DB('sqlite', 'testing.db')
+    current_db = DB('sqlite3', 'testing.db')
     current_db.connect()
     current_db.cur_gen()
-    with open('generate_tables.sql', 'r') as create:
-        current_db.create_table(create.read())
+    # Just for testing
+    # current_db.execute("DROP TABLE data;")
+    # current_db.execute("DROP TABLE defaults;")
+    # current_db.execute("DROP TABLE files;")
+    ##################
+    current_db.create_table('../generate_tables.sql')
     
-    current_db.insert("""INSERT INTO files VALUES (%s, %s);""", [filename, datetime.now()])
+    current_db.execute("""INSERT INTO files VALUES (%s, %s);""", [filename, datetime.now()])
     with open(filename, 'r') as kepler_file:
+        ## Remove this
+        lines = kepler_file.read().count('\n')
+        kepler_file.seek(0)
+        temp = 0
+        ## Remove ^
         for line in kepler_file:
+            ##########
+            temp += 1
+            print("{}/{}".format(temp,lines))
+            ##########
+
             # Handles header data
-            if line[0] == "\\":
-                line = line[1:].split("=")
+            if line[0] == """\\""":
+                line = line[1:].strip().split(" = ")
+                ##########
+                print(line)
+                ##########
                 if "KEPLERID" == line[0]:
                     kep_id = line[1]
                 elif "QUARTER" == line[0]:
@@ -35,13 +53,14 @@ def into_db(filename):
                 line.insert(0, quarter)
                 line.insert(0, kep_id)
                 # Shove it into the database
-                current_db.insert("""INSERT INTO data VALUES
+                current_db.execute("""INSERT INTO data VALUES
                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                     %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", line)
+                     %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""", line)
 
 
 
 if __name__ == '__main__':
     from sys import argv
+    print(argv)
     into_db(argv[1])
     print('Completed')
