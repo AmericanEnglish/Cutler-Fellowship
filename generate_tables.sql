14b02d3d1bd4ed6f6f1cfba57559f2e5550d812e
Carlos Barajas <12barajasc@gmail.com>
2016-01-24 00:18:40 -0500

    Need to adjust the NUMERIC values for specfic cases

--- generate_tables.sql
+++ generate_tables.sql
@@ -1,45 +1,46 @@
 CREATE TABLE files
 (
-    filename VARCHAR(20),
+    filename VARCHAR(50),
     datetime TIMESTAMP,
     PRIMARY KEY (filename)
 );
 
 CREATE TABLE defaults
 (
-    kepler_id INTEGER,
-    quarter INTEGER,
+    filename VARCHAR(50),
     name VARCHAR(12),
     value VARCHAR(10),
-    PRIMARY KEY (kepler_id, quarter)
+    PRIMARY KEY (filename, name),
+    FOREIGN KEY (filename)
+        REFERENCES files (filename)
 );
 
 CREATE TABLE data
 (
-    kepler_id INTEGER,
-    quarter INTEGER,
-    time REAL,
-    time_bjd REAL,
-    timecorr REAL,
+    -- PRECISE, DECIMALS RELEVANT
+    filename VARCHAR(50),
+    time NUMERIC(30, 15),
+    time_bjd NUMERIC(30, 15),
+    timecorr NUMERIC(30, 15),
     cadenceno INTEGER,
-    sap_flux REAL,
-    sap_flux_err REAL,
-    sap_bkg REAL,
-    sap_bkg_err REAL,
-    pdcsap_flux REAL,
-    pdcsap_flux_err REAL,
+    sap_flux NUMERIC(30, 15),
+    sap_flux_err NUMERIC(30, 15),
+    sap_bkg NUMERIC(30, 15),
+    sap_bkg_err NUMERIC(30, 15),
+    pdcsap_flux NUMERIC(30, 15),
+    pdcsap_flux_err NUMERIC(30, 15),
     sap_quality INTEGER,
-    psf_centr1 REAL,
-    psf_centr1_err REAL,
-    psf_centr2 REAL,
-    psf_centr2_err REAL,
-    mom_centr1 REAL,
-    mom_centr1_err REAL,
-    mom_centr2 REAL,
-    mom_centr2_err REAL,
-    pos_corr1 REAL,
-    pos_corr2 REAL,
-    PRIMARY KEY (kepler_id, quarter, time),
-    FOREIGN KEY (kepler_id, quarter)
-        REFERENCES defaults (kepler_id, quarter)
+    psf_centr1 NUMERIC(30, 15),
+    psf_centr1_err NUMERIC(30, 15),
+    psf_centr2 NUMERIC(30, 15),
+    psf_centr2_err NUMERIC(30, 15),
+    mom_centr1 NUMERIC(30, 15),
+    mom_centr1_err NUMERIC(30, 15),
+    mom_centr2 NUMERIC(30, 15),
+    mom_centr2_err NUMERIC(30, 15),
+    pos_corr1 NUMERIC(30, 15),
+    pos_corr2 NUMERIC(30, 15),
+    PRIMARY KEY (filename, time),
+    FOREIGN KEY (filename)
+        REFERENCES files (filename)
 );
