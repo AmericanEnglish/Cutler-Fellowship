fe710f1adc5e12861bb5423afb047a1636756d12
Carlos Barajas <12barajasc@gmail.com>
2016-01-24 01:09:33 -0500

    Removed messy debug lines small table adjustments before production needed

--- testlabs.py
+++ testlabs.py
@@ -3,38 +3,23 @@ from datetime import datetime
 from time import sleep
 
 def into_db(filename):
-    current_db = DB('sqlite3', 'cutler.db')#, host='localhost', user='student', password='student')
+    current_db = DB(db_type, db_name, host='localhost', user='student', password='student')
     current_db.connect()
     current_db.cur_gen()
-    # Just for testing
-    current_db.execute("DROP TABLE data;")
-    current_db.execute("DROP TABLE defaults;")
-    current_db.execute("DROP TABLE files;")
-    ##################
-    success = current_db.create_table('../generate_tables.sql')
+    # Saved for later debugging
+    # success = current_db.create_table('../generate_tables.sql')
     if not success[0]:
         print(success[1])
         exit()
-    else:
-        print('CREATED DATA TABLES')
-    sleep(3)
+    # else:
+    #     print('CREATED DATA TABLES')
 
     
     current_db.execute("""INSERT INTO files VALUES (%s, %s);""", [filename, datetime.now()])
     with open(filename, 'r') as kepler_file:
-        ## Remove this
-        lines = kepler_file.read().count('\n')
-        kepler_file.seek(0)
-        temp = 0
-        ## Remove ^
         for line in kepler_file:
-            ##########
-            temp += 1
-            print("{}/{}".format(temp,lines))
-            ##########
-
             # Handles header data
-            if line[0] == """\\""":
+            if line[0] == "\\":
                 line = line[1:].strip().split(" = ")
                 if len(line) == 1:
                     line.append('')
@@ -43,9 +28,7 @@ def into_db(filename):
             
             # Skips the three table headers
             elif "|" in line:
-                print('skipped')
-                # continue
-                # line = line[1:-1].replace("|"," ").split()
+                continue
             
             # Handles the EOF and perhaps bizzare line breaks
             elif len(line) < 5:
@@ -54,6 +37,7 @@ def into_db(filename):
             else:
                 # Insert data into the database
                 line = line.split()
+                # Sanatize read data for insertion
                 for index, item in enumerate(line):
                     if item == 'null':
                         line[index] = None
@@ -61,8 +45,8 @@ def into_db(filename):
                         line[index] = int(item)
                     else:
                         line[index] = float(item)
+                # Get the filename in there
                 line.insert(0, filename)
-                print(line)
                 # Shove it into the database
                 success = current_db.execute("""INSERT INTO data VALUES
                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
