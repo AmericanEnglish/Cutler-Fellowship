e61a26385380d5e78bd272c3b8854dc15e07d1b3
Carlos Barajas <12barajasc@gmail.com>
2016-01-25 21:29:41 -0500

    A working plot and name function

--- testlabs.py
+++ testlabs.py
@@ -61,19 +61,19 @@ def into_db(filename):
                 #     print(success[1])
     return current_db
 
-def graph(Database, item1, item2):
+def graph(Database, item1, item2, quarter):
     Database.cur_gen()
     ############### THIS IS SUPER NOT OK
     query = "SELECT %s, %s FROM data;" % (item1, item2)
     print(query)
     Database.execute(query)
-
+    ###############
     
     data = Database.fetchall()
     print(data[0:5])
     x, y = zip(*data)
     pyplot.plot(x,y)
-    pyplot.savefig("plot{}.png".format(datetime.now()))
+    pyplot.savefig("plot{}.png".format(datetime.now()).replace(' ', '-'))
 
 if __name__ == '__main__':
     from sys import argv
