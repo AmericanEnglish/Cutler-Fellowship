c84ea9af19d82c069d83cea18b53651206cba607
Carlos Barajas <12barajasc@gmail.com>
2016-01-26 18:39:25 -0500

    Finished into_db_dvseries

--- clean.py
+++ clean.py
@@ -59,6 +59,62 @@ def into_db_timeseries(current_db, directory, filename):
                     print(success[1])
     return current_db
 
+
+def into_db_dvseries(current_db, directory, filename):
+    current_db.cur_gen()
+    # Saved for later debugging
+    # else:
+    #     print('CREATED DATA TABLES')
+
+    
+    current_db.execute("""INSERT INTO files VALUES (%s, %s);""", [filename, datetime.now()])
+    with open("{}{}".format(directory, filename), 'r') as kepler_file:
+        for line in kepler_file:
+            # Handles header data
+            if line[0] == "\\":
+                if "=" not in line:
+                    continue
+                line = line[1:].strip().split(" = ")
+                if len(line) == 1:
+                    line.append('')
+                # Removes the double string formatting that can be present
+                if line[1].count("'") == 2:
+                    line[1] = line[1][1:-1]
+                # Cleans up extra spacing that may exist
+                for index, item in enumerate(line):
+                    line[index] = item.strip()
+                # print(line)
+                current_db.execute("""INSERT INTO dv_defaults VALUES (%s, %s, %s)""", [filename, line[0], line[1]])
+            
+            # Skips the three table headers
+            elif "|" in line:
+                continue
+            
+            # Handles the EOF and perhaps bizzare line breaks
+            elif len(line) < 5:
+                continue
+            # The important stuff
+            else:
+                # Insert data into the database
+                line = line.split()
+                # Sanatize read data for insertion
+                for index, item in enumerate(line):
+                    if item == 'NaN':
+                        line[index] = None
+                    elif '.' not in item:
+                        line[index] = int(item)
+                    else:
+                        line[index] = float(item)
+                # Get the filename in there
+                line.insert(0, filename)
+                # Shove it into the database
+                success = current_db.execute("""INSERT INTO dv_data VALUES
+                    (%s, %s, %s, %s, %s, %s, %s, %s);""", line)
+                if not success[0]:
+                    print(success[1])
+    return current_db
+
+
 def create_tables(database, tablesql):
     database = DB('postgres', 'cutler', host='localhost', user='student', password='student')
     database.cur_gen()
