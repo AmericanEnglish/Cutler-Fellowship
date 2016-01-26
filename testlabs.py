d5121eea09506da50d129bf2aa24e5da368720dd
Carlos Barajas <12barajasc@gmail.com>
2016-01-26 12:52:37 -0500

    Working graph!!!!

--- testlabs.py
+++ testlabs.py
@@ -1,26 +1,56 @@
 from database import DB
 from datetime import datetime
 from matplotlib import pyplot
-from clean import into_db
-def graph(Database, x, y, quarter):
+from os import listdir
+from clean import *
+
+def graph(Database, x, y, series_type):
     Database.connect()
     Database.cur_gen()
     ############### THIS IS SUPER NOT OK
-    query = """SELECT {}, {} 
-            FROM data INNER JOIN defaults ON
-                (data.filename = defaults.filename)
-            WHERE defaults.name = 'QUARTER' 
-                AND defaults.value = %s;""".format(x, y)
-    Database.execute(query, (quarter,))
+    query = """SELECT {0}, {1} 
+            FROM {2}_data""".format(x, y, series_type)
+    print(query)
+    Database.execute(query)
     ###############
     
     data = Database.fetchall()
     x, y = zip(*data)
-    pyplot.plot(x,y)
+    pyplot.scatter(x,y)
     pyplot.xlabel(x)
     pyplot.ylabel(y)
+    pyplot.tight_fit()
     pyplot.savefig("plot{}.png".format(datetime.now()).replace(' ', '-'))
+
+
+def into_db_DVseries(Database, filename):
+    pass
+
+def query_builder(arguments):
+    """(dict of strs) -> str
+
+        Takes a dictionary of arguments and constructs and SQL query to be fed
+        into a database. Below is a bare minimum dictionary required.
+
+        sample = {
+            x:'x_name',
+            y:'y_name',
+            series_type:'time/DV'
+        }
+        >>> query_builder(sample)
+        'SELECT x_name, y_name FROM time/DV_data;"""
+
 
 if __name__ == '__main__':
     from sys import argv
-    graph(DB('postgres', 'cutler', host='localhost', user='student', password='student'), 'time', 'sap_flux', '2')
+    basebase = DB('postgres', 'cutler', host='localhost', user='student', password='student')
+    print(basebase)
+    basebase.connect()
+    basebase.cur_gen()
+    if len(argv) < 3:
+        create_tables(basebase, 'generate_tables.sql')
+        for item in listdir(argv[1]):
+            if 'kplr' in item:
+                into_db_timeseries(basebase, argv[1], item)
+    else:
+        graph(basebase, argv[1][:-1], argv[2][:-1], argv[3])
