0244c56573bcf56aa9d20cd41e735a67f27f1955
Carlos Barajas <12barajasc@gmail.com>
2016-03-24 13:13:32 -0400

    changed the db model

--- SampleQueries.sql
+++ SampleQueries.sql
@@ -1,5 +1,5 @@
 SELECT time, sap_flux 
-FROM data INNER JOIN defaults ON
+FROM time_data INNER JOIN defaults ON
     (data.filename = defaults.filename)
 WHERE defaults.name = 'QUARTER' 
     AND defaults.value = '1';
